64c02c9c83a2299421e91e2fd64fc060eb092392
Julia1505 <makcika@inbox.ru>
2022-04-22 10:01:25 +0300

    done

--- hw7_1/hw7.cpp
+++ hw7_1/hw7.cpp
@@ -22,12 +22,10 @@ public:
     ~ArrayOfString();
 
     string operator[](int i);
-    void resize(int newSize);
     string *get(){return array;}
     int arSize(){return size;}
-//    void deleteAr();
-void growSize();
 
+    void growSize();
 
     friend std::istream& operator >> (std::istream &in, ArrayOfString &array);
     friend std::ostream& operator << (std::ostream &out, ArrayOfString &array);
@@ -53,8 +51,6 @@ void ArrayOfString::growSize() {
 std::istream& operator >> (std::istream &in, ArrayOfString &array) {
     int i = 0;
     while (in >> array.array[i]) {
-//        if (array.array[i] == "0")
-//            break;
         ++i;
         ++array.size;
         if (array.size == array.capacity) {
@@ -62,39 +58,6 @@ std::istream& operator >> (std::istream &in, ArrayOfString &array) {
         }
 
     }
-
-//    array.resize(10);
-//    int i = 0;
-//    int count = 0;
-//    int size = 10;
-//    while (in >> array.array[i]) {
-////        std::cout << array.array[i] << "\n";
-//        ++i;
-//        ++array.size;
-//        if (array.size == array.capacity) {
-//            array.resize(array.capacity * 2);
-//        }
-
-
-//        if (array.size == array.capacity) {
-//            size *= 2;
-//            array.resize(size );
-//        }
-
-
-//    array.resize(10);
-//    char buffer[100];
-//    int i = 0;
-//    while (in >> buffer) {
-//        if (array.size == array.capacity) {
-//            array.resize(array.capacity * 4);
-//        }
-//
-//        array.array[i] = buffer;
-//        std::cout << array[i];
-//        ++i;
-//        ++array.size;
-//    }
 
     return in;
 }
@@ -106,9 +69,6 @@ std::ostream& operator << (std::ostream &out, ArrayOfString &array) {
     return out;
 }
 
-
-
-
 ArrayOfString::~ArrayOfString() {
     delete[] array;
 }
@@ -116,42 +76,17 @@ ArrayOfString::~ArrayOfString() {
 ArrayOfString::ArrayOfString(ArrayOfString &copy) {
     size = copy.size;
     capacity = copy.capacity;
-//    delete[] array;
     array = new string[capacity];
     for (int i = 0; i < copy.size; ++i) {
         array[i] = copy[i];
     }
 }
 
-//void ArrayOfString::deleteAr() {
-//    delete[] array;
-//}
-
-void ArrayOfString::resize(int newSize) {
-    ArrayOfString temp(*this);
-//    temp = *this;
-
-
-    capacity = newSize;
-    delete[] array;
-    array = new string[capacity];
-
-    for (int i = 0;i < size; ++i) {
-        array[i] = temp[i];
-    }
-
-}
-
-
 void MSD(string *array, int j, int l, int r) {
     int size = r - l + 1;
-//    for (int i = l; i <= r;++i ) {
-//        std::cout<<array[i]<<" ";
-//    }
+
     auto *alfavit = new int[256];
-//    for (int i =0; i < 256; ++i) {
-//        alfavit[i]=0;
-//    }
+
     memset(alfavit, 0, 256 * 4);
 
     for (int i = l; i <= r; ++i) {
@@ -161,50 +96,28 @@ void MSD(string *array, int j, int l, int r) {
             ++alfavit[(int) array[i][j]];
         }
     }
-//    for (int i = 0; i < 255;++i){
-//        std::cout<<(int)alfavit[i]<<" ";
-//    }
-//    std::cout<<"\n";
 
     for (int i = 1; i < 256; ++i) {
         alfavit[i] += alfavit[i - 1];
     }
-//    for (int i = 0; i < 255;++i){
-//        std::cout<<(int)alfavit[i]<<" ";
-//    }
 
-//    ArrayOfString sortA(size);
     auto *sort = new string[size];
 
-
-//    std::cout<<"\n\n"<<size<<"\n";
     for (int  i = r; i >= l; --i) {
-//        std::cout<<array[i]<<" \n";
         sort[--alfavit[array[i][j]]] = array[i];
-//        std::cout<<sort[alfavit[array[i][j]]]<<" ";
     }
 
-//    std::cout<<"\nsort:  ";
-//    for (int i = 0; i < sortA.arSize();++i){
-//        std::cout<<sort[i]<<" ";
-//    }
+    delete[] alfavit;
 
-//    delete[] alfavit;
-
-    for (int i = l; i < size; ++i) {
-        array[i] = sort[i];
+    for (int i = l; i <= r; ++i) {
+        array[i] = sort[i - l];
     }
 
-//    std::cout<<"\n";
-////
-//    for (int i = l;i<=r;++i){
-//        std::cout<<array[i]<<" ";
-//    }
-//        std::cout<<"\n";
+    delete[] sort;
 
     for (int i = l; i <= r; ++i) {
         int count = 0;
-        while (i < r && array[i][j] == array[i + 1][j]) {
+        while (i < r && array[i][j] == array[i + 1][j] && array[i][j] != '\0') {
             ++count;
             ++i;
         }
@@ -218,16 +131,9 @@ void MSD(string *array, int j, int l, int r) {
 int run(std::istream& input, std::ostream& output) {
     ArrayOfString a;
     input >> a;
-//    std::cout<<a;
-//    ArrayOfString b = a;
-//    a.resize(20);
-//    std::cout << a;
-
 
     MSD(a.get(), 0, 0, a.arSize() - 1);
-//    output << a;
     std::cout << a;
-
 
     return 0;
 }
